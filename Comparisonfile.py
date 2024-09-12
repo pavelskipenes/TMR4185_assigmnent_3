9fec00d77bce40ecd9e7af115b964fa52fa076bc
Torfinn <Torfinf@stud.ntnu.no>
2024-09-12 14:12:41 +0200

    fullført opp til slutt av del 1

--- Comparisonfile.py
+++ Comparisonfile.py
@@ -19,3 +19,36 @@ tmax = 400  # time duration, s
 t = (np.arange(0, tmax, h))
 P = np.zeros(len(t))  # initialize load vector to zero
 
+#Verdier spess for harmonic load
+omega = 0.4  # frequency
+amp = 1  # amplitude
+eps1 = np.pi  # phase angle
+P = amp * np.cos(omega * t + eps1)
+
+# Student code called here!
+u = const_avg_acc(m, k, c, P, h, u0, udot0)
+urk = runge_kutta4(m, k, c, P, h, u0, udot0)
+uss = dis_state_space(m, k, c, P, h, u0, udot0)
+uas = voldsfunksjon(c, m, u0, udot0, k, P[0], omega, t, eps1)
+
+# Plot
+plt.figure(1)
+plt.subplot(211)
+plt.plot(t, P)
+plt.grid(True)
+plt.xlabel('time, s')
+plt.ylabel('P, N')
+plt.xlim((0, tmax))
+
+plt.subplot(212)
+plt.plot(t, u, 'k', label='CAA')
+plt.plot(t, urk, 'b--', label='RK')
+plt.plot(t, uss, 'r--', label='DSS')
+plt.plot(t, uas, 'y--', label='VOLD')
+plt.legend()
+plt.grid(True)
+plt.xlabel('time, s')
+plt.ylabel('u, m')
+# Dette burde ha dekt deloppgave 1 innen kode.
+
+#går så over til del 2
