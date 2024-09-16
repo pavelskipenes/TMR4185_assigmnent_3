afcc30014599f32b3387f99b4a2b55372ad3950e
Pavel Skipenes <38912521+pavelskipenes@users.noreply.github.com>
2024-09-16 22:50:00 +0200

    format code (#18)

--- dis_state_space.py
+++ dis_state_space.py
@@ -1,30 +1,26 @@
 import numpy as np
 
-def dis_state_space(m,k,c,P,h,u0,udot0):
-    n=len(P)    #fordi P er en n lang vektor
-    
-    u=np.zeros(n)
-    udot=np.zeros(n)
 
-    u[0]=u0
-    udot[0]=udot0
-    
-    x=np.array([u,udot])
-    A=np.array([[0,1],[-k/m,-c/m]])   #matriseform for newton 2.L
-    B=np.array([[0],[1]])               #bare mhp last for x_2 derivert->akselerasjon
+def dis_state_space(m, k, c, P, h, u0, udot0):
+    n = len(P)  # fordi P er en n lang vektor
 
-    #tar ikke hensyn til C, ettersom forholdstallet bare er 1.
+    u = np.zeros(n)
+    udot = np.zeros(n)
+
+    u[0] = u0
+    udot[0] = udot0
+
+    x = np.array([u, udot])
+    A = np.array([[0, 1], [-k/m, -c/m]])  # matriseform for newton 2.L
+    B = np.array([[0], [1]])  # bare mhp last for x_2 derivert->akselerasjon
+
+    # tar ikke hensyn til C, ettersom forholdstallet bare er 1.
 
     for i in range(1, n):
-        dx=A@x + B*P[i-1]
-        x=x+dx*h
+        dx = A@x + B*P[i-1]
+        x = x+dx*h
 
-        u[i]=x[0][0]
-        udot[i]=x[0][1]
+        u[i] = x[0][0]
+        udot[i] = x[0][1]
 
     return u
-
-
-
-        
-    
